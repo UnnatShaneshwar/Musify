92f5b103c65cf165f625ec076451b5f44d5840a3
Unnat Shaneshwar <unnat@archo.localdomain>
2021-04-29 17:58:21 +0530

    More changes

--- src/components/Song.js
+++ src/components/Song.js
@@ -3,8 +3,8 @@ import React from "react";
 const Song = ({ currentSong }) => {
   return (
     <>
-      <div className="song-container flex items-center justify-center flex-col">
-        <img src={currentSong.cover} alt={currentSong.name} className="w-1/5 rounded-full" />
+      <div className="flex items-center justify-center flex-col">
+        <img src={currentSong.cover} alt={currentSong.name} className="w-full rounded-full" />
         <h1 className="pt-5 font-semibold text-4xl">{currentSong.name}</h1>
         <h3 className="">{currentSong.artist}</h3>
       </div>
